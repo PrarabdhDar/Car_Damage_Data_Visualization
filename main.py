993340131ccff3e57e2ae7eef4c19151d1c96796
PrarabdhDar <prarabdhdar12@gmail.com>
2022-04-10 02:16:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import os
 import json
 import sys
 
+from util import polygon, bb
+
 #Taking input data
 if(len(sys.argv) == 0):
     image_path = 'Assignment/Data Visualization/images/3.jpg'
@@ -75,33 +77,6 @@ for d in data:
         top.append((int(x_min), int(y_min)))
 
 
-#First image Visualization function
-def polygon(image_path, data):
-    image =  image = cv2.imread(image_path)
-    height, width = image.shape[:2]
+polygon(image_path, data)
 
-    color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
-
-    overlay = image.copy()
-
-    for p in pts:
-        image = cv2.polylines(image, np.int32([p]),
-                            True, color, 3)
-        cv2.fillPoly(overlay, np.int32([p]), color)
-        poly_image = cv2.addWeighted(overlay, opacity, image, 1-opacity, 0)
-
-        return asarray(poly_image)
-
-#Second Image Visualization function
-def bb(image_path, data):
-    image =  image = cv2.imread(image_path)
-    height, width = image.shape[:2]
-
-    for i in range(num):
-        color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
-        cv2.rectangle(image, start[i], end[i], color, 2)
-        bottom.append((top[i][0] + 10*len(label[i]), top[i][1]))
-        cv2.rectangle(image, (top[i][0], top[i][1] - 15), bottom[i], (0,0,0), -1)
-        cv2.putText(image, label[i], top[i], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
-
-    return asarray(image)
+bb(image_path, data)
